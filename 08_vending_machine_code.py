0a38fe5f0b17b5583a640abf6b0a303e47def4a0
devirrived <devirrived@outlook.com>
2022-08-20 00:54:45 -0300

    Adiciona 11 projetos do curso (playground)

---
+++ 08_vending_machine_code.py
@@ -0,0 +1,19 @@
+class Soda_Machine:
+    paid = False
+    balance = 0
+
+def eject_soda(self, paid):
+    if paid == False:
+      print('Please insert money')
+    else:
+      print('Enjoy the soda!')
+
+def pay(self, amount):
+    self.balance += amount
+    
+def select_soda(self, balance):
+    if balance >= 1:
+      self.paid = True
+      self.balance -= 1
+    else:
+      self.paid = False
